ddba10fbecd805e21640a589b310d917fb9aa203
lechicong <lechicong.reddevil.1610@gmail.com>
2021-11-11 17:13:58 +0700

    Chapter 8: Basic login

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
     root 'static_pages#home'
 
     get "static_pages/home"
-    get "static_pages/help"
-    get "users/show"
+    get "/help", to: "static_pages#help"
     get "/signup", to: "users#new"
     post "/signup", to: "users#create"
     resources :users, only: %i(new show create)
+    get "/login", to: "sessions#new"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
   end
 end
