e566912b478a0c29a00b726d928c9c01391fc033
lechicong <lechicong.reddevil.1610@gmail.com>
2021-11-12 11:21:26 +0700

    Chapter 9: Advanced login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_09_092303) do
+ActiveRecord::Schema.define(version: 2021_11_12_030500) do
 
   create_table "admins", force: :cascade do |t|
     t.string "name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2021_11_09_092303) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
